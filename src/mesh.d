f393e190a732a09cbf6d1ef4eb89a0965018726c
Martijn van Iersel <mvaniersel@gmail.com>
2025-06-07 23:14:35 +0200

    Textured sphere

--- src/mesh.d
+++ src/mesh.d
@@ -1,13 +1,26 @@
 module mesh;
 
 import helix.util.vec;
+import std.algorithm;
+import std.array;
+import helix.allegro.bitmap;
+
+struct Vertex {
+	vec3f position;
+	vec2f texCoord;
+
+	this(vec3f position, vec2f texCoord) {
+		this.position = position;
+		this.texCoord = texCoord;
+	}
+}
 
 struct Mesh {
-	vec3f[] vertices;
+	Vertex[] vertices;
 	int[3][] faces;
 
 	this(vec3f[] vertices, int[3][] faces) {
-		this.vertices = vertices;
+		this.vertices = vertices.map!(v => Vertex(v, vec2f(0.0f, 0.0f))).array;
 		this.faces = faces;
 	}
 }
